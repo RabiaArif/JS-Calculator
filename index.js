f5c16ac9d235c869cab4d3ef6a0cbeecb54921fe
RabiaArif <rabia59malik@gmail.com>
2022-08-23 23:29:19 +0500

    Adding bootstrap grid and js file with js function eval() but it is not recomemded because of security risks. Read index.js file

--- index.js
+++ index.js
@@ -2,15 +2,25 @@
 function clearScreen() {
     document.getElementById("result").value = "";
 }
- 
+
 // This function display values
 function display(value) {
     document.getElementById("result").value += value;
 }
- 
+
 // This function evaluates the expression and returns result
 function calculate() {
     var p = document.getElementById("result").value;
-    var q = eval(p);
+    var q = eval(p);            //  The eval() method evaluates or executes an argument.
+                                // If the argument is an expression, 
+                                // eval() evaluates the expression. 
+                                // If the argument is one or more JavaScript statements, 
+                                // eval() executes the statements.
+                                
+                                // But use of eval is not preferable beacause of security risk. 
+                                // With eval(), malicious code can run inside your application without permission.
+                                // With eval(), third-party code can see the scope of your application, 
+                                // which can lead to possible attacks.
+
     document.getElementById("result").value = q;
 }
